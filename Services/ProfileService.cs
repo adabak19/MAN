9f432434e6f9b51a1c3fe8b19f8c70126c19eafa
marcintyminski <mtyminski8@gmail.com>
2024-11-12 10:47:42 +0100

    profile service

---
+++ Services/ProfileService.cs
@@ -0,0 +1,37 @@
+using System.Collections.Specialized;
+using MAN.Models;
+
+namespace MAN.Services;
+
+public static class ProfileService{
+    static List<Profile> Profiles {get;}
+    static int nextId = 4;
+    static ProfileService(){
+        Profiles = new List<Profile>
+        {
+            new Profile { Id = 1, FirstName = "Jan", LastName = "Tolkien", ProfileName = "jpjp"},
+            new Profile { Id = 1, FirstName = "Jan", LastName = "Tolkieniak", ProfileName = "jpjpjp"},
+            new Profile { Id = 1, FirstName = "John", LastName = "Tolkienowski", ProfileName = "jpjp2"}
+        };
+    }
+
+    public static List<Profile> GetAll() => Profiles;
+
+    public static Profile? Get(int id) => Profiles.FirstOrDefault(a => a.Id == id);
+    public static void Add(Profile profile){
+        profile.Id = nextId++;
+        Profiles.Add(profile);
+    }
+    public static void Delete(int id){
+        var profile = Get(id);
+        if(profile is null)
+            return;
+        Profiles.Remove(profile);
+    }
+    public static void Update(Profile profile){
+        var index = Profiles.FindIndex(a => a.Id == profile.Id);
+        if(index == -1)
+            return;
+        Profiles[index] = profile;
+    }
+}
