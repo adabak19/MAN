4abd66c4dc2734fc31bba0c46ec45fd2af2f7082
nat <91956093+natalkawajda@users.noreply.github.com>
2024-11-21 13:22:42 +0100

    Enhance data models with key attributes and relationships for BookRead, CoAuthors, and BookGenre

--- Models/BookRead.cs
+++ Models/BookRead.cs
@@ -1,11 +1,17 @@
 namespace MAN.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class BookRead
 {
+    [Key]
     public int ProfileId {get; set;}
+    public Profile? Profile {get; set;}
+    [Key]
     public int BookId {get;set;}
+    public Book? Book {get; set;}
     public int Rating {get; set;}
     public DateOnly DateStarted {get; set;}
     public DateOnly DateFinished {get; set;}
-    public bool Status {get; set;}
+    [MaxLength(7)]
+    public string? Status {get; set;}
 }
