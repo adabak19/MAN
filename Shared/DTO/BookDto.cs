82470dac662844ad8ba492efddbc19c72963b85a
marcintyminski <mtyminski8@gmail.com>
2024-11-27 15:53:14 +0100

    Books and single book components co-authors: nat and Ada

--- Shared/DTO/BookDto.cs
+++ Shared/DTO/BookDto.cs
@@ -2,7 +2,7 @@ namespace MAN.Shared.DTO;
 
 public class BookDto
 {
-
+    public int Id {get; set;}
     public string? ISBN {get; set;}
     public string? Title { get; set; }
     public string? AuthorName { get; set; }
