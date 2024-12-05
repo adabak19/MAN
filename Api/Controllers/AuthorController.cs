6c2e3c1139accec07e6bd9ecfc0ff9a23fc7d983
adabak19 <91956344+adabak19@users.noreply.github.com>
2024-12-05 18:57:13 +0100

    book and author management features with new services and UI updates

--- Api/Controllers/AuthorController.cs
+++ Api/Controllers/AuthorController.cs
@@ -11,40 +11,58 @@ namespace MAN.Api.Controllers;
 public class AuthorController : ControllerBase
 {
     private readonly IAuthorService _authorService;
-    public AuthorController(IAuthorService authorService){
+    public AuthorController(IAuthorService authorService)
+    {
         _authorService = authorService;
     }
     [HttpGet]
-    public async Task<IActionResult> GetAll(){
+    public async Task<IActionResult> GetAll()
+    {
         var authors = await _authorService.GetAllAsync();
         return Ok(authors);
     }
     [HttpGet("{id}")]
-    public async Task<ActionResult<AuthorDto>> Get(int id){
+    public async Task<ActionResult<AuthorDto>> Get(int id)
+    {
         var author = await _authorService.GetAsyncById(id);
         if (author is null)
             return NotFound();
         return author;
     }
     [HttpPost]
-    public async Task<IActionResult> Create(Author author){
+    public async Task<IActionResult> Create(Author author)
+    {
         await _authorService.Add(author);
-        return CreatedAtAction(nameof(Get), new { id = author.Id}, author);
+        return CreatedAtAction(nameof(Get), new { id = author.Id }, author);
     }
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update(int id, Author author){
+    public async Task<IActionResult> Update(int id, Author author)
+    {
         var existingAuthor = await _authorService.GetAsyncById(id);
-        if(existingAuthor is null)
+        if (existingAuthor is null)
             return NotFound();
         await _authorService.Update(author);
         return NoContent();
     }
     [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id){
+    public async Task<IActionResult> Delete(int id)
+    {
         var author = _authorService.GetAsyncById(id);
         if (author is null)
             return NotFound();
         await _authorService.Delete(id);
         return NoContent();
+    }
+    [HttpGet("author")]
+    public async Task<ActionResult<List<BookDto>>> SearchAuthorAsync([FromQuery] string? name)
+    {
+        var authors = await _authorService.SearchAuthorsAsync(name);
+        return Ok(authors);
+    }
+    
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllAuthors(){
+        var authors = await _authorService.GetAllAuthors();
+        return Ok(authors);
     }
 }
